ff93307c51f378adf67da00737c3cdf70744c4e7
GUSTAVO KUNDE SILVEIRA <16204041@pucrs.br>
2021-04-03 01:14:51 -0300

    inserindo tabela de personagens

---
+++ Persistencia/Entidades/ActorMovie.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Persistencia.Entidades
+{
+    public class ActorMovie
+    {
+        public int ActorMovieId { get; set; }
+       
+        public string CharacterName { get; set; }
+
+        [ForeignKey("MovieId")]
+        public int MovieId { get; set; }
+
+        public Movie Movie { get; set; }
+
+        [ForeignKey("ActorId")]
+        public int ActorId { get; set; }
+
+        public Actor Actor { get; set; }
+    }
+}
